f0bcb36c1c953262ecc2d6f314c39ab3088747e2
KomronSharpist <komrondeveloper@gmail.com>
2023-04-30 00:39:43 +0500

    Added controllers

---
+++ src/backend/PentaGOL.Api/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PentaGOL.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseController : Controller
+{
+}
---
+++ src/backend/PentaGOL.Api/Controllers/MatchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PentaGOL.Api.Controllers;
+
+public class MatchController : BaseController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
